08bee9457b98a6acd616501b995b376ad638dab3
Clark <81452611+freezenleo@users.noreply.github.com>
2021-06-28 17:22:28 -0500

    html function

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length); //const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log("=============");
-    //is the same as this
-    profileDataArr.forEach(profileItem =>
-        console.log(profileItem)
-    );
-}
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log("=============");
+//     //is the same as this
+//     profileDataArr.forEach(profileItem =>
+//         console.log(profileItem)
+//     );
+// }
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage());
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
