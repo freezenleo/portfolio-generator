81d5acc92d5ba6a48ef31b109217238c6cafc8b5
Clark <81452611+freezenleo@users.noreply.github.com>
2021-06-29 12:48:57 -0500

    inquirer function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length); //const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+
 
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
@@ -24,9 +27,18 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
